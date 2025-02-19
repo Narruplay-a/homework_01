b64da4738785a18c630fb9b85f9b762d20e51956
Adel Khaziakhmetov <adel.khaziakhmetov@shtrafy-gibdd.ru>
2025-02-19 20:23:36 +0300

    adel khaziakhmetov home 1

--- src/test/kotlin/ru/otus/homework/SumoftwoTest.kt
+++ src/test/kotlin/ru/otus/homework/SumoftwoTest.kt
@@ -8,22 +8,22 @@ class SumoftwoTest {
 
     @Test
     fun `testcase 1`() {
-        val actual = sumOfTwo(intArrayOf(2, 7, 11, 15), 9)
-        val expected = intArrayOf(0, 1)
+        val actual = sumOfTwo(arrayOf(2, 7, 11, 15), 9)
+        val expected = arrayOf(0, 1)
         assertArrayEquals(expected, actual)
     }
 
     @Test
     fun `testcase 2`() {
-        val actual = sumOfTwo(intArrayOf(3, 2, 4), 6)
-        val expected = intArrayOf(1, 2)
+        val actual = sumOfTwo(arrayOf(3, 2, 4), 6)
+        val expected = arrayOf(1, 2)
         assertArrayEquals(expected, actual)
     }
 
     @Test
     fun `testcase 3`() {
         assertThrows<IllegalArgumentException> {
-            sumOfTwo(intArrayOf(3, 2), 6)
+            sumOfTwo(arrayOf(3, 2), 6)
         }
     }
 }
