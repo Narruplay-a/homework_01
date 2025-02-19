db539bca103bd6269e0a560631545b71b1da883b
Adel Khaziakhmetov <adel.khaziakhmetov@shtrafy-gibdd.ru>
2025-02-19 20:13:24 +0300

    adel khaziakhmetov home 1

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -1,6 +1,19 @@
 package ru.otus.homework
 
+fun fizzbuzz(size: Int): Array<String> {
+    val stringArray = Array(size) { "" }
 
-fun fizzbuzz(n: Int): Array<String> {
-    TODO("Выполните задание")
+    for(i in 0..<size) {
+        stringArray[i] = if (i % 15 == 0) {
+            "FizzBuzz"
+        } else if (i % 3 == 0) {
+            "Fizz"
+        } else if (i % 5 == 0) {
+            "Buzz"
+        } else {
+            "$i"
+        }
+    }
+
+    return stringArray
 }
--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -1,6 +1,11 @@
 package ru.otus.homework
 
+fun sumOfTwo(array: Array<Int>, target: Int): Array<Int> {
+    for (i in 0..<array.size - 1) {
+        for (j in i + 1..<array.size) {
+            if (array[i] + array[j] == target) return arrayOf(i, j)
+        }
+    }
 
-fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
-    TODO("Выполните задание")
+    throw IllegalArgumentException("No value")
 }
